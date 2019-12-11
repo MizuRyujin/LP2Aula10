151c546d41fde8e80ab243f2552324150b0f2c70
Miguel Fernandez <miguelrf95@gmail.com>
2019-12-11 17:18:24 +0000

    Add new project to test creating custom indexers

---
+++ Indexers/MyVector.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Indexers
+{
+    struct MyVector
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public MyVector(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public float this[int index]
+        {
+            get
+            {
+                if (index == 0)
+                {
+                    return X;
+                }
+                else if (index == 1)
+                {
+                    return Y;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                
+            }
+
+            set
+            {
+                if (index == 0)
+                {
+                    X = value;
+                }
+                else if (index == 1)
+                {
+                    Y = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+        public float this[string index]
+        {
+            get
+            {
+                string idx = "xa0";
+                string idy = "yb1";
+                if (idx.Contains(index.ToLower()))
+                {
+                    return X;
+                }
+                else if (idy.Contains(index.ToLower()))
+                {
+                    return Y;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+            }
+
+            set
+            {
+                string idx = "xa0";
+                string idy = "yb1";
+                if (idx.Contains(index.ToLower()))
+                {
+                    X = value;
+                }
+                else if (idy.Contains(index.ToLower()))
+                {
+                    Y = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+    }
+}
---
+++ Indexers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyVector v1 = new MyVector(3, 5);
+            MyVector v2 = new MyVector(70, 5);
+
+            Console.WriteLine($"vector 1 = {v1.X}; vector 2 = {v2.Y}");
+
+            v1[0] = 90;
+            v2[1] = 30;
+
+            Console.WriteLine($"vector 1 = {v1.X}; vector 2 = {v2.Y}");
+
+            v1["a"] = 1;
+            v2["y"] = 1000;
+            
+            Console.WriteLine($"vector 1 = {v1.X}; vector 2 = {v2.Y}");
+        }
+    }
+}
