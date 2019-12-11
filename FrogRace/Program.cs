d05172f7f46b9c45a699c2cc94db8efd3b46d435
Miguel Fernandez <miguelrf95@gmail.com>
2019-12-11 16:15:52 +0000

    Add project files.

---
+++ FrogRace/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace FrogRace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread frog1 = new Thread(FrogRace);
+            Thread frog2 = new Thread(FrogRace);
+            Thread frog3 = new Thread(FrogRace);
+            frog1.Start(1);
+            frog2.Start(2);
+            frog3.Start(3);
+            frog1.Join();
+            frog2.Join();
+            frog3.Join();
+        }
+
+        static void FrogRace(object frog)
+        {
+            int nFrog = (int)frog;
+
+            Random rnd = new Random(nFrog + nFrog * 2);
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"Frog {nFrog} leaped {i} times");
+
+                Thread.Sleep(rnd.Next(1000));
+            }
+
+        }
+    }
+}
